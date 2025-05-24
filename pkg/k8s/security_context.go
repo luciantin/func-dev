18905b5cf32b9ef1da56ddb908b87c9671657b5c
Tin <lord66tin66@gmail.com>
2025-05-24 18:39:01 +0200

    Tekton PVC Issue, cant delete old build file in pipeline "/tmp/volume_mnt/source/.s2i/*"

--- pkg/k8s/security_context.go
+++ pkg/k8s/security_context.go
@@ -8,35 +8,66 @@ import (
 
 var oneTwentyFour = semver.MustParse("1.24")
 
+//func defaultPodSecurityContext() *corev1.PodSecurityContext {
+//	// change ownership of the mounted volume to the first non-root user uid=1000
+//	if IsOpenShift() {
+//		return nil
+//	}
+//	runAsUser := int64(1001)
+//	runAsGroup := int64(1002)
+//	return &corev1.PodSecurityContext{
+//		RunAsUser:  &runAsUser,
+//		RunAsGroup: &runAsGroup,
+//		FSGroup:    &runAsGroup,
+//	}
+//}
+//
+//func defaultSecurityContext(client *kubernetes.Clientset) *corev1.SecurityContext {
+//	runAsNonRoot := true
+//	sc := &corev1.SecurityContext{
+//		Privileged:               new(bool),
+//		AllowPrivilegeEscalation: new(bool),
+//		RunAsNonRoot:             &runAsNonRoot,
+//		Capabilities:             &corev1.Capabilities{Drop: []corev1.Capability{"ALL"}},
+//		SeccompProfile:           nil,
+//	}
+//	if info, err := client.ServerVersion(); err == nil {
+//		var v *semver.Version
+//		v, err = semver.NewVersion(info.String())
+//		if err == nil && v.Compare(oneTwentyFour) >= 0 {
+//			sc.SeccompProfile = &corev1.SeccompProfile{Type: corev1.SeccompProfileTypeRuntimeDefault}
+//		}
+//	}
+//	return sc
+//}
+
 func defaultPodSecurityContext() *corev1.PodSecurityContext {
-	// change ownership of the mounted volume to the first non-root user uid=1000
-	if IsOpenShift() {
-		return nil
-	}
-	runAsUser := int64(1001)
-	runAsGroup := int64(1002)
+	zero := int64(0)
 	return &corev1.PodSecurityContext{
-		RunAsUser:  &runAsUser,
-		RunAsGroup: &runAsGroup,
-		FSGroup:    &runAsGroup,
+		RunAsUser:  &zero,
+		RunAsGroup: &zero,
+		FSGroup:    &zero,
 	}
 }
 
 func defaultSecurityContext(client *kubernetes.Clientset) *corev1.SecurityContext {
-	runAsNonRoot := true
+	runAsNonRoot := false
+	zero := int64(0)
+
 	sc := &corev1.SecurityContext{
+		RunAsNonRoot:             &runAsNonRoot,
+		RunAsUser:                &zero,
+		RunAsGroup:               &zero,
 		Privileged:               new(bool),
 		AllowPrivilegeEscalation: new(bool),
-		RunAsNonRoot:             &runAsNonRoot,
 		Capabilities:             &corev1.Capabilities{Drop: []corev1.Capability{"ALL"}},
-		SeccompProfile:           nil,
 	}
+
 	if info, err := client.ServerVersion(); err == nil {
-		var v *semver.Version
-		v, err = semver.NewVersion(info.String())
-		if err == nil && v.Compare(oneTwentyFour) >= 0 {
+		if v, err := semver.NewVersion(info.String()); err == nil && v.Compare(oneTwentyFour) >= 0 {
 			sc.SeccompProfile = &corev1.SeccompProfile{Type: corev1.SeccompProfileTypeRuntimeDefault}
 		}
 	}
+
 	return sc
 }
